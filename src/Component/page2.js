580ff756991424569e34a0665f80577c8f71cdd5
unknown <chandana.m@terralogic.com>
2020-12-28 18:07:30 +0530

    react1

---
+++ src/Component/page2.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import "./style.scss"
+
+
+export class page2 extends Component {
+    render() {
+        return (
+            <div className="page2">
+            <h1>This is my Page2 component</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default page2
+
